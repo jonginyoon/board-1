856b558b7b7f7e769a1ff1a420c6272f7a8b3210
이종학 <jhlee@jhlee.local>
2019-06-03 15:24:11 +0900

    Article repository에 이름을 이용한 쿼리 method 생성

--- src/main/java/com/example/board/repository/ArticleRepository.java
+++ src/main/java/com/example/board/repository/ArticleRepository.java
@@ -1,7 +1,12 @@
 package com.example.board.repository;
 
 import com.example.board.model.Article;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    List<Article> findByTitleContains(String title, Sort sort);
 }
